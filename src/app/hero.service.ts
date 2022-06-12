7b66045878b6f20b90b72f2ff8045c8046adf452
Abdullah Ahmed <abdullah.work06@gmail.com>
2022-06-12 11:51:00 +0200

    adding the message service & observing the heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-app';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
-  getHeroes():Hero[]{
-    return HEROES
+  constructor(private _MessageService: MessageService) { }
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this._MessageService.add('Heroeservice: fetched heroes ')
+    return heroes;
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
+import { MessageService } from './../message.service';
 import { HEROES } from './../mock-app';
 import { Hero } from './../hero';
 import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -16,15 +18,17 @@ export class HeroesComponent implements OnInit {
   heroes:Hero[] = [];
   // inject the service
   // the paramerter simultaneously defines a private parameter property and identifies it as a service parameter injection site .
-  constructor(private _HeroService: HeroService) { }
+  constructor(private _HeroService: HeroService, private _MessageService: MessageService) { }
 
   ngOnInit(): void {
-
-    this.heroes = this._HeroService.getHeroes();
+    this.getHeroes()
   }
   onSelect(hero:Hero):void{
     this.selectedHero = hero;
-    console.log(this.selectedHero);
+    this._MessageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+  getHeroes():void{
+    this._HeroService.getHeroes().subscribe(heroes => this.heroes = heroes)
   }
 
 }
